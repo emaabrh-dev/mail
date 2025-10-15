b4bca89e1ef04f42646f92486ec80ead76a69ec7
Harinavalona-web <andriamampandryharinavalona@gmail.com>
2025-10-15 10:25:29 +0300

    Update accordion.tsx

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,4 +1,16 @@
+'use client'
 
+import * as React from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { ChevronDownIcon } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
+function Accordion({
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Root>) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+}
 function AccordionItem({
   className,
   ...props
@@ -51,3 +63,4 @@ function AccordionContent({
 }
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+
